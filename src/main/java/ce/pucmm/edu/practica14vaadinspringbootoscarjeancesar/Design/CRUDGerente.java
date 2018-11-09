346c7100adef446bd8d0c751756306ffab1b9b15
Jean Louis Tejeda García <jeanlotejeda0101@hotmail.com>
2018-11-09 14:15:28 -0400

    Agregando usuarios.

--- src/main/java/ce/pucmm/edu/practica14vaadinspringbootoscarjeancesar/Design/CRUDGerente.java
+++ src/main/java/ce/pucmm/edu/practica14vaadinspringbootoscarjeancesar/Design/CRUDGerente.java
@@ -1,6 +1,5 @@
 package ce.pucmm.edu.practica14vaadinspringbootoscarjeancesar.Design;
 
-
 import ce.pucmm.edu.practica14vaadinspringbootoscarjeancesar.Services.UsuarioService;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.FontAwesome;
@@ -17,17 +16,14 @@ public class CRUDGerente extends UI {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    PantallaCrearUsuario pantallaCrearUsuario;
+    TextField nombre = new TextField("Nombre:");
+    TextField email = new TextField("Email:");
+    PasswordField contrasena = new PasswordField("Contraseña:");
 
-    Button enviar = new Button("Crear");
+    Button agregar = new Button("Salvar");
+    Button cancelar = new Button("Cancelar");
 
     private VerticalLayout verticalLayout = new VerticalLayout();
-
-    @Autowired
-    public CRUDGerente(PantallaCrearUsuario pantallaCrearUsuario) {
-        this.pantallaCrearUsuario = pantallaCrearUsuario;
-    }
 
     @Override
     protected void init(VaadinRequest request) {
@@ -37,52 +33,52 @@ public class CRUDGerente extends UI {
             getUI().getPage().setLocation("/");
         else {
             configurarPagina();
-            agregarHeader();
 
-            pantallaCrearUsuario = new PantallaCrearUsuario();
-
-            enviar.addStyleName(ValoTheme.BUTTON_PRIMARY);
-            enviar.setIcon(FontAwesome.PLUS);
-            configuraBotonPantalla(enviar, "Agregar nuevo gerente", pantallaCrearUsuario);
-
-            verticalLayout.addComponent(enviar);
-
+            nombre.setValue("");
+            email.setValue("");
+            contrasena.setValue("");
         }
-
     }
 
     private void configurarPagina() {
         Page.getCurrent().setTitle("Practica #14 - OCJ - CRUD");
 
+        agregar.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        agregar.setIcon(FontAwesome.SAVE);
+
+        agregar.addClickListener((evento) -> {
+            try {
+                usuarioService.crearUsuario(usuarioService.contarUsuario(), nombre.getValue(), email.getValue(), contrasena.getValue());
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+
+            nombre.setValue("");
+            email.setValue("");
+            contrasena.setValue("");
+        });
+
+        cancelar.addClickListener((evento) -> {
+            nombre.setValue("");
+            email.setValue("");
+            contrasena.setValue("");
+        });
+
+        Label header = new Label("CRUD de Gerentes");
+        header.addStyleName(ValoTheme.LABEL_H1);
+        header.setSizeUndefined();
+
+        HorizontalLayout botoneslayout = new HorizontalLayout(agregar, cancelar);
+        botoneslayout.setSpacing(true);
+
+        nombre.setCaption("Nombre: ");
+        email.setCaption("Email: ");
+        contrasena.setCaption("Contraseña: ");
+
         verticalLayout = new VerticalLayout();
         verticalLayout.setSizeFull();
         verticalLayout.setDefaultComponentAlignment(Alignment.TOP_CENTER);
         setContent(verticalLayout);
-    }
-
-    private void agregarHeader() {
-        Label header = new Label("CRUD de Gerentes");
-        header.addStyleName(ValoTheme.LABEL_H1);
-        header.setSizeUndefined();
-        verticalLayout.addComponent(header);
-    }
-
-    private void abrirPantalla(String title, FormLayout form) {
-        Window vistaPantalla = new Window(title);
-
-        vistaPantalla.center();
-        vistaPantalla.setResizable(false);
-        vistaPantalla.setModal(true);
-        vistaPantalla.setClosable(true);
-        vistaPantalla.setDraggable(false);
-        vistaPantalla.setContent(form);
-
-        addWindow(vistaPantalla);
-    }
-
-    private void configuraBotonPantalla(Button boton, String titulo, FormLayout formulario) {
-        boton.addClickListener((e) -> {
-            abrirPantalla(titulo, formulario);
-        });
+        verticalLayout.addComponents(header, nombre, email, contrasena, botoneslayout);
     }
 }
